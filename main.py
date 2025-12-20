854fafbab645f3a48e02493425a927837c408f0e
Mahasvan <mahasvan@gmail.com>
2025-12-20 21:12:09 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import json
 
-from src.pysysinfo.dumps.windows.windows_dump import WindowsHardwareManager
+from src.pysysinfo.dumps.mac.mac_dump import MacHardwareManager
 import time
 
 start_time = time.time()
-lhm = WindowsHardwareManager()
+lhm = MacHardwareManager()
 
 lhm.fetch_cpu_info()
 lhm.fetch_memory_info()
-# lhm.fetch_storage_info()
+lhm.fetch_storage_info()
 end_time = time.time()
 print(f"Time taken: {end_time - start_time} seconds")
-# fetch_memory_info()
 
 json_data = json.loads(lhm.info.model_dump_json())
 
-# with open("response.json", "w") as f:
-    # json.dump(json_data, f, indent=2)
+with open("response.json", "w") as f:
+    json.dump(json_data, f, indent=2)
 
 print(json.dumps(json_data, indent=2))
 # print("done")
