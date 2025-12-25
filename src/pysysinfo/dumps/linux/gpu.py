74b1b4e5a7920477e119110751a2e5531cf59280
Mahasvan <mahasvan@gmail.com>
2025-12-25 13:38:24 +0530

    WIP: use /sys/bus/pci/devices/ for GPU

--- src/pysysinfo/dumps/linux/gpu.py
+++ src/pysysinfo/dumps/linux/gpu.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from src.pysysinfo.dumps.linux.common import pci_from_acpi_linux
 from src.pysysinfo.models.gpu_models import GPUInfo, GraphicsInfo
@@ -9,38 +10,58 @@ from src.pysysinfo.models.status_models import FailedStatus, PartialStatus
 # https://unix.stackexchange.com/questions/393/how-to-check-how-many-lanes-are-used-by-the-pcie-card
 
 def fetch_gpu_info() -> GraphicsInfo:
-    gpu_info = GraphicsInfo()
+    graphics_info = GraphicsInfo()
 
-    if not os.path.exists("/sys/class/drm"):
-        gpu_info.status = FailedStatus("/sys/class/drm does not exist")
-        # WSL for example does not have this.
-        return gpu_info
+    ROOT_PATH = "/sys/bus/pci/devices/"
 
-    for file in os.listdir("/sys/class/drm/"):
-        # DRM devices (not FBDev) are enumerated with the format `cardX`
-        # inside of sysfs's DRM directory. So we look for those, and traverse
-        # them. We look for the `device` and `vendor` file, which should always be there.
-        if ("card" in file) and ("-" not in file):
-            gpu = GPUInfo()
-            try:
-                path = f"/sys/class/drm/{file}/device"
-                ven = open(f"{path}/vendor", "r").read().strip()
-                dev = open(f"{path}/device", "r").read().strip()
-                gpu.vendor_id = ven
-                gpu.device_id = dev
+    if not os.path.exists(ROOT_PATH):
+        graphics_info.status = FailedStatus("/sys/bus/pci/devices/ not found")
+        return graphics_info
 
-                acpi_path, pci_path = pci_from_acpi_linux(path)
-                if acpi_path:
-                    gpu.acpi_path = acpi_path
-                if pci_path:
-                    gpu.pci_path = pci_path
+    for device in os.listdir(ROOT_PATH):
+        # print("Found device: ", device)
+        try:
+            path = os.path.join(ROOT_PATH, device)
+            device_class = open(os.path.join(path, "class")).read().strip()
+            """
+            The class code is three hex-bytes, where the leftmost hex-byte is the base class
+            We want the devices of base class 0x03, which denotes a Display Controller.
+            """
+            class_code = int(device_class, base=16)
 
-                gpu_info.modules.append(gpu)
-
-            except Exception as e:
-                gpu_info.status = PartialStatus(messages=gpu_info.status.messages)
-                gpu_info.status.messages.append(f"Failed to get detail for {file}: {e}")
+            base_class = class_code >> 16
+            if base_class != 3:
                 continue
+        except Exception as e:
+            graphics_info.status = FailedStatus(f"Could not open file for {device}: {e}")
+            return graphics_info
 
-    return gpu_info
+        gpu = GPUInfo()
 
+        try:
+            gpu.vendor_id = open(os.path.join(path, "vendor")).read().strip()
+            gpu.device_id = open(os.path.join(path, "device")).read().strip()
+        except Exception as e:
+            graphics_info.status = PartialStatus(messages=graphics_info.status.messages)
+            graphics_info.status.messages.append(f"Could not get vendor or device id: {e}")
+
+        try:
+            lspci_output = subprocess.run(["lspci", "-s", device, "-vmm"], capture_output=True, text=True).stdout
+            # We gather all data here and parse whatever data we have. Subsystem data may not be returned.
+            data = {}
+            for line in lspci_output.splitlines():
+                if ":" in line:
+                    key, value = line.split(':', 1)
+                    data[key.strip()] = value.strip()
+
+            gpu.manufacturer = data.get("Vendor")
+            gpu.model = data.get("Model")
+            gpu.subsystem_manufacturer = data.get("SVendor")
+            gpu.subsystem_model = data.get("SDevice")
+        except Exception as e:
+            graphics_info.status = PartialStatus(messages=graphics_info.status.messages)
+            graphics_info.status.messages.append(f"Could not parse LSPCI output for GPU {device}: {e}")
+
+        graphics_info.gpus.append(gpu)
+
+    return graphics_info
--- src/pysysinfo/models/gpu_models.py
+++ src/pysysinfo/models/gpu_models.py
@@ -3,21 +3,25 @@ from typing import Optional, List
 from pydantic import BaseModel
 
 from src.pysysinfo.models.component_model import ComponentInfo
+from src.pysysinfo.models.size_models import StorageSize
+
 
 class GPUInfo(BaseModel):
 
     model: Optional[str] = None
+
+    vendor_id: Optional[str] = None
+    device_id: Optional[str] = None
+
     manufacturer: Optional[str] = None
+    subsystem_manufacturer: Optional[str] = None
+    subsystem_model: Optional[str] = None
 
     acpi_path: Optional[str] = None
     pci_path: Optional[str] = None
 
-    # HDD/SSD
-    type: Optional[str] = None
-
-    device_id: Optional[str] = None
-    vendor_id: Optional[str] = None
-
+    pcie_width: Optional[int] = None
+    vram: Optional[StorageSize] = None
     pass
 
 class GraphicsInfo(ComponentInfo):
