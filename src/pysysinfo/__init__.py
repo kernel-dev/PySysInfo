65ca9a7e72dfa0ce83ae40dcf9bc00332ebfb89e
Mahasvan <mahasvan@gmail.com>
2025-12-29 21:58:58 +0530

    Add dynamic properties

--- src/pysysinfo/__init__.py
+++ src/pysysinfo/__init__.py
@@ -1,6 +1,10 @@
 import os
 import platform
 
+__version__ = "0.0.1"
+__author__ = "Mahasvan"
+__license__ = "BSD-3-Clause"
+
 if os.name == "nt":
     from pysysinfo.dumps.windows.windows_dump import WindowsHardwareManager as HardwareManager
 elif platform.system() == "Darwin":
