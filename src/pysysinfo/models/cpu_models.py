3b5cf195ac41b8c4ad184025c2635edc7a4894b6
Mahasvan <mahasvan@gmail.com>
2025-12-31 20:00:07 +0530

    Add docstrings for CPUInfo

--- src/pysysinfo/models/cpu_models.py
+++ src/pysysinfo/models/cpu_models.py
@@ -4,11 +4,26 @@ from pysysinfo.models.component_model import ComponentInfo
 
 
 class CPUInfo(ComponentInfo):
+    #: This is the CPU's name
     model_name: Optional[str] = None
+    
+    #: The CPU's architecture, x86, ARM, etc.
     architecture: Optional[str] = None
+    
+    #: Denotes whether the CPU is 32 or 64 bit. 
+    #: Independent of whether the OS is 32 or 64 bit.
     bitness: Optional[int] = None
+    
+    #: ARM Version. Will be ``null`` on x86 CPUs.
     arch_version: Optional[str] = None
+    
+    #: Manufacturer of the CPU. ``Intel``, for example.
     vendor: Optional[str] = None
+    
+    #: SSE flags supported by the CPU.
     sse_flags: List[str] = []
+    
+    #: The number of physical cores present on the CPU
     cores: Optional[int] = None
+    #: The number of logical threads supported by the CPU
     threads: Optional[int] = None
